9370b7a8f37e3007e7e724920d832864c5db4a41
Alexey Ten (Lynn) <alexeyten@gmail.com>
2021-12-13 00:30:28 +0300

    day12

---
+++ day12/day12p2.js
@@ -0,0 +1,51 @@
+import { readFileSync } from 'fs';
+
+const nodes = readFileSync('./input12.txt', 'utf8')
+    .split('\n')
+    .filter(Boolean)
+    .map(s => s.split('-'))
+    .reduce((nodes, [a, b], i) => {
+        nodes[a] = nodes[a] ?? { name: a, small: /[a-z]/.test(a), links: new Set() };
+        nodes[b] = nodes[b] ?? { name: b, small: /[a-z]/.test(b), links: new Set() };
+        if (b !== 'start' && a !== 'end') nodes[a].links.add(b);
+        if (a !== 'start' && b !== 'end') nodes[b].links.add(a);
+        return nodes;
+    }, {});
+
+let n = 0;
+visit('start');
+console.log(n);
+
+function visit(name, visited = new Set(), list = [], twice = '') {
+    const node = nodes[name];
+
+    if (node.small) {
+        if (visited.has(name)) {
+            if (twice) return;
+            twice = name;
+        } else {
+            visited.add(name);
+        }
+    }
+
+    list.push(name);
+
+    for (const next of node.links) {
+        if (next === 'end') {
+            n++;
+            // console.log(list.concat(next).join());
+            continue;
+        }
+        visit(next, visited, list, twice);
+    }
+
+    list.pop();
+
+    if (node.small) {
+        if (twice === name) {
+            twice = '';
+        } else {
+            visited.delete(name);
+        }
+    }
+}
